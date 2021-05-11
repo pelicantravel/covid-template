511a246877bc6e402ca8dd2c3b7d33ced73e4cd2
Damián Dlugosch <damian@damiandlugosch.com>
2021-05-11 12:26:06 +0200

    Update covid collection points

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 const couponSection = document.querySelector('.exchange-coupon__section');
 const covidTestType = document.querySelector('.rychlotest-type__section');
-const slinnyPCRSection = document.querySelector('.rychlotest-slinny__section');
-const nosohltanPCRSection = document.querySelector('.rychlotest-nosohltan__section');
+const digitalParkSection = document.querySelector('.rychlotest-digitalpark__section');
+const skyParkSection = document.querySelector('.rychlotest-skypark__section');
 
-const btnSlinnyPCR = document.querySelector('.rychlotest-type__section a:first-of-type');
-const btnNosohltanPCR = document.querySelector('.rychlotest-type__section a:last-of-type');
+const btnDigitalPark = document.querySelector('.rychlotest-type__section a:first-of-type');
+const btnSkyPark = document.querySelector('.rychlotest-type__section a:last-of-type');
 
 const btsBtn = document.querySelector('#chooseBTS');
 const anotherBtn = document.querySelector('#chooseAnother');
@@ -23,38 +23,44 @@ function hideSection(name) {
 
 btsBtn.addEventListener('click', () => {
   btsBtn.classList.replace('uk-button-default', 'uk-button-primary');
+  btsBtn.classList.add('active');
   if (!couponSection.hasAttribute('hidden')) {
     anotherBtn.classList.replace('uk-button-primary', 'uk-button-default');
+    anotherBtn.classList.remove('active');
     couponSection.setAttribute('hidden', '');
   }
-  hideSection(slinnyPCRSection);
-  hideSection(nosohltanPCRSection);
+  hideSection(digitalParkSection);
+  hideSection(skyParkSection);
   showSection(covidTestType);
 });
 
 anotherBtn.addEventListener('click', () => {
   anotherBtn.classList.replace('uk-button-default', 'uk-button-primary');
+  anotherBtn.classList.add('active');
   if (!covidTestType.hasAttribute('hidden')) {
     btsBtn.classList.replace('uk-button-primary', 'uk-button-default');
+    btsBtn.classList.remove('active');
     hideSection(covidTestType);
   }
-  if (!slinnyPCRSection.hasAttribute('hidden')) {
+  if (!digitalParkSection.hasAttribute('hidden')) {
     btsBtn.classList.replace('uk-button-primary', 'uk-button-default');
-    hideSection(slinnyPCRSection);
+    btsBtn.classList.remove('active');
+    hideSection(digitalParkSection);
   }
-  if (!nosohltanPCRSection.hasAttribute('hidden')) {
+  if (!skyParkSection.hasAttribute('hidden')) {
     btsBtn.classList.replace('uk-button-primary', 'uk-button-default');
-    hideSection(nosohltanPCRSection);
+    btsBtn.classList.remove('active');
+    hideSection(skyParkSection);
   }
   showSection(couponSection);
 });
 
-btnSlinnyPCR.addEventListener('click', () => {
+btnDigitalPark.addEventListener('click', () => {
   hideSection(covidTestType);
-  showSection(slinnyPCRSection);
+  showSection(digitalParkSection);
 });
 
-btnNosohltanPCR.addEventListener('click', () => {
+btnSkyPark.addEventListener('click', () => {
   hideSection(covidTestType);
-  showSection(nosohltanPCRSection);
+  showSection(skyParkSection);
 });
